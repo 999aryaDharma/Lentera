2f342f4c9c6f5695dfe36957b82551f2805a9130
KrisnaMahardika <gedekrisna119@gmail.com>
2024-11-05 00:08:25 +0800

    Update admin page 2

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,7 +33,7 @@
         </div>
     </a>
 
-    <a href="" class=" h-32 shadow-md border-2 rounded-lg flex justify-center place-items-center gap-2 px-5">
+    <a href="{{route('order')}}" class=" h-32 shadow-md border-2 rounded-lg flex justify-center place-items-center gap-2 px-5">
         <svg class="w-28 h-28 text-slate-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clipboard-list">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
